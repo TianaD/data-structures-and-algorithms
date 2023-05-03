86acb245530005da3648e41911e03202ba905307
TianaD <tldavis99@yahoo.com>
2023-05-02 21:58:25 -0500

    challenge03 complete

--- javascript/code-challenges/challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges/challenges-07.test.js
@@ -46,9 +46,7 @@ CHALLENGE 3
 Write a function named joinArray that takes an array and joins all of the elements together in one string on a space.
 ------------------------------------------------------------------------------------------------ */
 
-const joinArray = (arr) => {
-  // Solution code here...
-};
+const joinArray = (arr) => arr.join(' ');
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
