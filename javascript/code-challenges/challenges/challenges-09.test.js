4ae950b40e4df324e5a69e6cd1d32c747a925286
TianaD <tldavis99@yahoo.com>
2023-05-09 21:48:23 -0500

    challenge05 complete

--- javascript/code-challenges/challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges/challenges-09.test.js
@@ -124,7 +124,9 @@ const characters = [
 
 const getHouses = (arr) => {
   let houses = [];
-  // Solution code here...
+  arr.forEach(element => {
+    houses.push(element.house)
+  })
   return houses;
 };
 
